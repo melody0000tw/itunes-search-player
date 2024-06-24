ae6f0d63aa9019c2cf587a8492e6318547d35f78
Yun Chieh Lee <melody0000tw@gmail.com>
2024-06-24 22:29:41 +0800

    fix long description auto sizing layout

--- itunes-search-player/itunes-search-player/Views/ViewController.swift
+++ itunes-search-player/itunes-search-player/Views/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
     private var dataSource: UICollectionViewDiffableDataSource<Section, Track>!
     
     private var datas: [Track] = [
-        Track(trackId: 1, trackName: "你好", trackTimeMillis: 123, longDescription: "你好", artworkURL100: "123", previewURL: "123"),
+        Track(trackId: 1, trackName: "你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好", trackTimeMillis: 123, longDescription: "你好", artworkURL100: "123", previewURL: "123"),
         Track(trackId: 2, trackName: "你好", trackTimeMillis: 123, longDescription: "你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好", artworkURL100: "123", previewURL: "123"),
-        Track(trackId: 3, trackName: "你好", trackTimeMillis: 123, longDescription: "你好", artworkURL100: "123", previewURL: "123"),
+        Track(trackId: 3, trackName: "你好你好你好", trackTimeMillis: 123, longDescription: nil, artworkURL100: "123", previewURL: "123"),
         Track(trackId: 4, trackName: "你好", trackTimeMillis: 123, longDescription: "你好", artworkURL100: "123", previewURL: "123"),
-        Track(trackId: 5, trackName: "你好", trackTimeMillis: 123, longDescription: "你好", artworkURL100: "123", previewURL: "123"),
+        Track(trackId: 5, trackName: "你好", trackTimeMillis: 123, longDescription: "你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好你好", artworkURL100: "123", previewURL: "123"),
     ]
     
     override func viewDidLoad() {
@@ -38,11 +38,10 @@ class ViewController: UIViewController {
     }
     
     private func configureLayout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(160.0))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(160))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
         
         let section = NSCollectionLayoutSection(group: group)
         return UICollectionViewCompositionalLayout(section: section)
@@ -52,13 +51,13 @@ class ViewController: UIViewController {
     private func configureDataSource() {
         dataSource = UICollectionViewDiffableDataSource<Section, Track>(collectionView: self.collectionView) {
             (collectionView, indexPath, track) -> UICollectionViewCell in
+            
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrackCell.reuseIdentifier, for: indexPath) as? TrackCell else {
                 return UICollectionViewCell()
             }
             
             cell.trackNameLabel.text = track.trackName
             cell.descriptionLabel.text = track.longDescription
-            
             
             return cell
         }
