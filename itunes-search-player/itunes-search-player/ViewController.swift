6f83b195e1836ae367dcb8bbf89651a5e45debcb
Yun Chieh Lee <melody0000tw@gmail.com>
2024-06-24 15:52:06 +0800

    create xcode project

---
+++ itunes-search-player/itunes-search-player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itunes-search-player
+//
+//  Created by Melody Lee on 2024/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
