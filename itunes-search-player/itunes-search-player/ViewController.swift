6fcca504d3173ed8b364760d5c208011562002e4
Yun Chieh Lee <melody0000tw@gmail.com>
2024-06-24 16:34:24 +0800

    create searchTextField UI layout & setup text change function

--- itunes-search-player/itunes-search-player/ViewController.swift
+++ itunes-search-player/itunes-search-player/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
-
+    
+    private func configureUI() {
+        searchButton.layer.cornerRadius = 6
+        searchButton.addTarget(self, action: #selector(search), for: .touchUpInside)
+        searchTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc func search() {
+        print("did tapped searchBtn")
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text else {
+            return
+        }
+        print("search text: \(text)")
+    }
 }
-
